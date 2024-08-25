0748ae20c5bbb127683b09882ba23862bd03f93d
Divya <divyanamdev8795@gmail.com>
2024-08-25 21:54:33 +0530

    fix:update text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <div className="flex flex-col items-center object-contain justify-center h-[100vh] overflow-hidden bg-no-repeat bg-cover bg-[url('./assets/heroimage-2.jpg')] ">
       <h1 className="font-poppins text-center text-black font-bold text-3rem sm:text-[40px]">
-        Find your fav Books & Authors
+        Find All your Favorite Books & Authors
       </h1>
       <SearchInput />
     </div>
